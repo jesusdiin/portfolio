067e19b855b11139e46cf3cf574dfa99a3802455
Jesús Hernández <cjesus02hdz@gmail.com>
2023-01-13 20:41:14 -0600

    feat(navbar): :sparkles: file header add
    Add file Header, build structure navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import './App.css'
+import './App.css';
+import Header from './components/header/Header';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
 
 
-
-export default App
+export default App;
