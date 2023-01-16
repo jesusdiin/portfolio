9d732d98287db7831ce8987f99f4c10217d4be08
Jesús Hernández <cjesus02hdz@gmail.com>
2023-01-16 12:49:40 -0600

    feat(home): :sparkles: add home section

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Social() {
+	return (
+		<div className='home_social'>
+			<a href="https://www.instagram.com/jesus.diin" className="home_social_icon" target='_blank'>
+				<i className="uil uil-instagram"></i>
+			</a>
+
+			<a href="#" className="home_social_icon" target='_blank'>
+				<i className="uil uil-linkedin"></i>
+			</a>
+
+			<a href="https://github.com/jesusdiin" className="home_social_icon" target='_blank'>
+				<i className="uil uil-github-alt"></i>
+			</a>
+			
+		</div>
+	)
+}
+
+export default Social
