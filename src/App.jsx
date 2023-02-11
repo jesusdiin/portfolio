28753b1538087f6a94c8706ecf6eeae3e9a50750
Jesús Hernández <cjesus02hdz@gmail.com>
2023-02-10 20:13:42 -0600

    feat(style): :sparkles: add section qualification

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 
 
 
@@ -18,6 +19,7 @@ const App = () => {
                 <About />
                 <Skills />
                 <Services />
+                <Qualification />
             </main>
         </>
     )
