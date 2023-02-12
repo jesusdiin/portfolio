0d4005fa109a9796defa1eab83d3e27e5a0e81f1
Jesús Hernández <cjesus02hdz@gmail.com>
2023-02-12 04:56:21 -0600

    feat(style): :sparkles: add button scroll

---
+++ src/components/scrollup/Scrollup.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './scrollup.css';
+
+export const Scrollup = () => {
+
+	window.addEventListener('scroll', function () {
+		const scrollUp = document.querySelector('.scrollup');
+		if (this.scrollY >= 560) scrollUp.classList.add('show_scroll');
+		else scrollUp.classList.remove('show_scroll')
+	});
+
+	return (
+		<a href="#" className="scrollup">
+			<i className="uil uil-arrow-up scrollup_icon"></i>
+		</a>
+	)
+}
+
+export default Scrollup
